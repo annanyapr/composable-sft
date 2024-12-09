de7ce8e5e27456312fa8c0d61b463e47b8e64cf0
annanyapr <annanyapr@gmail.com>
2024-12-08 19:22:00 -0500

    Updated evaluation scripts to use local lang SFT

--- examples/text-classification/eval_sa.sh
+++ examples/text-classification/eval_sa.sh
@@ -1,14 +1,27 @@
 #!/bin/bash
 
-# Check if language argument is provided
+# Check if at least one argument is provided
 if [ -z "$1" ]; then
-  echo "Usage: $0 <LANG> [TASK_FT]"
+  echo "Usage: $0 <LANG> [TASK_FT] OR $0 <LANG_FT_PATH> <LANG> [TASK_FT]"
   exit 1
 fi
 
-LANG=$1  # Required argument
-LANG_FT="cambridgeltl/xlmr-lang-sft-${LANG}-small"
-TASK_FT=${2:-"cambridgeltl/xlmr-task-sft-nusax_senti"}  # Optional, with a default value
+if [ -d "$1" ]; then
+  # Case: LANG_FT_PATH is provided
+  LANG_FT=$1
+  LANG=$2
+  TASK_FT=${3:-"cambridgeltl/xlmr-task-sft-nusax_senti"}  # Optional, with a default value
+else
+  # Case: LANG is provided
+  LANG=$1
+  LANG_FT="cambridgeltl/xlmr-lang-sft-${LANG}-small"
+  TASK_FT=${2:-"cambridgeltl/xlmr-task-sft-nusax_senti"}  # Optional, with a default value
+fi
+
+# Echo parameters
+echo "LANG_FT: $LANG_FT"
+echo "LANG: $LANG"
+echo "TASK_FT: $TASK_FT"
 
 RESULTS_DIR="results/NusaX-senti/${LANG}"
 mkdir -p $RESULTS_DIR
